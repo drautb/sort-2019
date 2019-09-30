6aaf26b34ac722091957065143630429f56120bd
Ben Draut <drautb@gmail.com>
2019-09-30 14:23:35 -0600

    Basic write examples

---
+++ src/main/java/io/github/drautb/App.java
@@ -0,0 +1,33 @@
+
+package io.github.drautb;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+
+  private static final String TABLE_NAME = "drautb-sort-2019";
+
+  private static final AmazonDynamoDB dynamoDbClient = AmazonDynamoDBClientBuilder
+      .standard()
+      .withRegion("us-east-1")
+      .withCredentials(new ProfileCredentialsProvider("fh2-us-east-1"))
+      .build();
+
+  public static void main(String[] args) {
+    Map<String, AttributeValue> item = new HashMap<>();
+    item.put("lock_name", new AttributeValue("test-lock"));
+    item.put("name", new AttributeValue("bob"));
+
+    PutItemRequest putItemRequest = new PutItemRequest(TABLE_NAME, item);
+
+    dynamoDbClient.putItem(putItemRequest);
+  }
+
+}
